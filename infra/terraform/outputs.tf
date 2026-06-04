b7f53b90acd75ff6af2d585356aa6cc4a5398af5
Gokdeniz <gokdeniz@Gokdeniz-Dizustu-Bilgisayar.local>
2026-06-05 00:26:17 +0300

    oidc

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -10,6 +10,3 @@ output "cognito_domain" {
   value = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${var.aws_region}.amazoncognito.com"
 }
 
-output "github_actions_role_arn" {
-  value = aws_iam_role.github_actions.arn
-}
