8756e82a589f3aa20032df291c2a82cf075b4e5f
Gokdeniz <gokdeniz@Gokdeniz-Dizustu-Bilgisayar.local>
2026-06-06 14:53:30 +0300

    feat: add get-profile endpoint, persist interests across sessions

--- app-backend/lambdas/users/update-interests/dist/index.js
+++ app-backend/lambdas/users/update-interests/dist/index.js
@@ -1,7 +1,31 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
 // lambdas/users/update-interests/index.ts
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
-var dynamo = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+var update_interests_exports = {};
+__export(update_interests_exports, {
+  handler: () => handler
+});
+module.exports = __toCommonJS(update_interests_exports);
+var import_client_dynamodb = require("@aws-sdk/client-dynamodb");
+var import_lib_dynamodb = require("@aws-sdk/lib-dynamodb");
+var dynamo = import_lib_dynamodb.DynamoDBDocumentClient.from(new import_client_dynamodb.DynamoDBClient({}));
 var USERS_TABLE_NAME = process.env.USERS_TABLE_NAME;
 var CORS_ORIGIN = process.env.CORS_ORIGIN ?? "*";
 var headers = {
@@ -30,7 +54,7 @@ var handler = async (event) => {
     }
     const now = (/* @__PURE__ */ new Date()).toISOString();
     await dynamo.send(
-      new UpdateCommand({
+      new import_lib_dynamodb.UpdateCommand({
         TableName: USERS_TABLE_NAME,
         Key: { PK: `USER#${userId}`, SK: "PROFILE" },
         UpdateExpression: "SET interests = :interests, updatedAt = :now, email = if_not_exists(email, :email)",
@@ -55,7 +79,8 @@ var handler = async (event) => {
     };
   }
 };
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   handler
-};
+});
 //# sourceMappingURL=index.js.map
