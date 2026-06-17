597b58eececf118bb8c5638155bc29a1381d3d62
Gokdeniz <gokdeniz@Gokdeniz-Dizustu-Bilgisayar.local>
2026-06-17 21:16:23 +0300

    google auth

--- infra/terraform/terraform.tfvars
+++ infra/terraform/terraform.tfvars
@@ -3,8 +3,6 @@ contact_email = "read@cogletta.com"
 app_name      = "Cogletta"
 ses_from_email = "read@cogletta.com"
 
-google_client_id     = ""
-google_client_secret = ""
 
 
 
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -66,12 +66,12 @@ variable "app_name" {
 variable "google_client_id" {
   description = "Google OAuth Client ID"
   type        = string
-  default     = ""
+  
 }
 
 variable "google_client_secret" {
   description = "Google OAuth Client Secret"
   type        = string
   sensitive   = true
-  default     = ""
+  
 }
