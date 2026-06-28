a85d2481fa51149abcdb93d627bf321bf86bd686
Gokdeniz <gokdeniz@Gokdeniz-Dizustu-Bilgisayar.local>
2026-06-28 21:46:13 +0300

    import tfstate

--- infra/terraform/ses.tf
+++ infra/terraform/ses.tf
@@ -12,12 +12,13 @@ resource "aws_ses_domain_dkim" "cogletta" {
 
 # Route53'e DKIM kayıtları ekle (otomatik)
 resource "aws_route53_record" "ses_dkim" {
-  count   = 3
-  zone_id = data.aws_route53_zone.cogletta.zone_id
-  name    = "${aws_ses_domain_dkim.cogletta.dkim_tokens[count.index]}._domainkey.cogletta.com"
-  type    = "CNAME"
-  ttl     = 300
-  records = ["${aws_ses_domain_dkim.cogletta.dkim_tokens[count.index]}.dkim.amazonses.com"]
+  count           = 3
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.cogletta.zone_id
+  name            = "${aws_ses_domain_dkim.cogletta.dkim_tokens[count.index]}._domainkey.cogletta.com"
+  type            = "CNAME"
+  ttl             = 300
+  records         = ["${aws_ses_domain_dkim.cogletta.dkim_tokens[count.index]}.dkim.amazonses.com"]
 
   lifecycle {
     ignore_changes = all
@@ -26,11 +27,12 @@ resource "aws_route53_record" "ses_dkim" {
 
 # SES domain verification TXT kaydı
 resource "aws_route53_record" "ses_verification" {
-  zone_id = data.aws_route53_zone.cogletta.zone_id
-  name    = "_amazonses.cogletta.com"
-  type    = "TXT"
-  ttl     = 300
-  records = [aws_ses_domain_identity.cogletta.verification_token]
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.cogletta.zone_id
+  name            = "_amazonses.cogletta.com"
+  type            = "TXT"
+  ttl             = 300
+  records         = [aws_ses_domain_identity.cogletta.verification_token]
 
   lifecycle {
     ignore_changes = all
@@ -39,11 +41,12 @@ resource "aws_route53_record" "ses_verification" {
 
 # MX kaydı — read@cogletta.com'a email alabilmek için
 resource "aws_route53_record" "mx" {
-  zone_id = data.aws_route53_zone.cogletta.zone_id
-  name    = "cogletta.com"
-  type    = "MX"
-  ttl     = 300
-  records = ["10 inbound-smtp.eu-central-1.amazonaws.com"]
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.cogletta.zone_id
+  name            = "cogletta.com"
+  type            = "MX"
+  ttl             = 300
+  records         = ["10 inbound-smtp.eu-central-1.amazonaws.com"]
 
   lifecycle {
     ignore_changes = all
@@ -52,11 +55,12 @@ resource "aws_route53_record" "mx" {
 
 # SPF kaydı — email deliverability
 resource "aws_route53_record" "spf" {
-  zone_id = data.aws_route53_zone.cogletta.zone_id
-  name    = "cogletta.com"
-  type    = "TXT"
-  ttl     = 300
-  records = ["v=spf1 include:amazonses.com ~all"]
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.cogletta.zone_id
+  name            = "cogletta.com"
+  type            = "TXT"
+  ttl             = 300
+  records         = ["v=spf1 include:amazonses.com ~all"]
 
   lifecycle {
     ignore_changes = all
@@ -65,11 +69,12 @@ resource "aws_route53_record" "spf" {
 
 # DMARC kaydı
 resource "aws_route53_record" "dmarc" {
-  zone_id = data.aws_route53_zone.cogletta.zone_id
-  name    = "_dmarc.cogletta.com"
-  type    = "TXT"
-  ttl     = 300
-  records = ["v=DMARC1; p=quarantine; rua=mailto:read@cogletta.com"]
+  allow_overwrite = true
+  zone_id         = data.aws_route53_zone.cogletta.zone_id
+  name            = "_dmarc.cogletta.com"
+  type            = "TXT"
+  ttl             = 300
+  records         = ["v=DMARC1; p=quarantine; rua=mailto:read@cogletta.com"]
 
   lifecycle {
     ignore_changes = all
